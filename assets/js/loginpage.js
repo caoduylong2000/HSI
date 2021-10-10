c4e5a04687f142610440ea982223e44f2e24591f
duylongcoder <duylongcoder@gmail.com>
2021-10-10 10:49:51 +0700

    HSI #2

--- assets/js/loginpage.js
+++ assets/js/loginpage.js
@@ -6,6 +6,6 @@ signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
 });
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
+// signInButton.addEventListener('click', () => {
+// 	container.classList.remove("right-panel-active");
+// });
